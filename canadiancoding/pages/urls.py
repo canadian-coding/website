08e03a27e36bd07b9deb5fb0290a5f0eb31a6e0c
Kieran Wood <kieranw098@gmail.com>
2019-11-21 00:33:19 -0700

    Added services page, added contact form, added more details to homepage, and smaller bug fixes.

--- canadiancoding/pages/urls.py
+++ canadiancoding/pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r"^$", views.index, name = "index"), # The site homepage
+    url(r"services", views.service, name = "services"), # The site homepage
 ]

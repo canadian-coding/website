e9c0edaf6b4958b94f1c9b30300bd70fa6681d0b
Kieran Wood <kieranw098@gmail.com>
2019-11-29 02:00:45 -0700

    Updated allowedhost for heroku

--- canadiancoding/settings.py
+++ canadiancoding/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ "canadian-coding-website.herokuapp.com"]
+ALLOWED_HOSTS = [ ".herokuapp.com"]
 
 
 # Application definition

c2e25fb493b314c89f0561d410c0c9bfbc9a6352
Kieran Wood <kieranw098@gmail.com>
2019-12-03 23:49:53 -0700

    Fixed view for post details by slug regex

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r"^$", views.index, name = "index"), # The posts homepage
     url(r"^post/(?P<id>\d+)/$", views.details, name = "details"), # Details page; accessed based on post ID
-    url(r"^post/(?P<language>\w+)/(?P<slug>\w+)/$", views.details_by_slug, name = "details_by_slug"), # Details page; accessed based on post language, and slug
+    url(r"^post/(?P<language>\w+)/(?P<slug>.*)/$", views.details_by_slug, name = "details_by_slug"), # Details page; accessed based on post language, and slug
 ]
